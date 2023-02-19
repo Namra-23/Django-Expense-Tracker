54b46bce7bd57c83a5ebaabb6a2eb3b6d2954c97
Namra-23 <namrajoshi2303@gmail.com>
2023-02-19 16:32:37 +0530

    form

--- track_expense/forms.py
+++ track_expense/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Expense,User
+from django import forms
 
 class ExpenseForm(ModelForm):
     class Meta:
@@ -7,11 +8,13 @@ class ExpenseForm(ModelForm):
         fields = ('name','amount','category')
 
 class UserForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ('name','gmail','password','phone')
 
 class LoginForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ('gmail','password')
--- track_expense/urls.py
+++ track_expense/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.login,name='login'),
     path('edit/<int:id>/',views.edit,name='edit'),
     path('delete/<int:id>/',views.delete,name='delete'),
     path('register/',views.register,name='register'),
